a54b016eabde8abe7a6d68304bd17ba1da1c3781
Sergio <shadow41402@gmail.com>
2023-12-01 19:18:16 -0800

    Added the textbookInfoCopier func, only prints, working with comments

--- main.js
+++ main.js
@@ -6,14 +6,12 @@ const fs = require("fs");
 const path = require("path");
 const puppeteer = require("puppeteer");
 
-// const rootPath = path.join(__dirname, "..");
-// const filePath = path.join(rootPath, "csv_files", "souTextbooksList.csv");
+const rootPath = path.join(__dirname, "..");
+const filePath = path.join(rootPath, "csv_files", "souTextbooksList.csv");
 
 // 55 courses total
 // ART has 15 courses
 // ART has a total of 18 sections for all courses
-
-// !!Instead of overloading the page with 1000 entries, get one department's info, then refresh and start over with the next department!!
 
 const page_url = "https://sou.bncollege.com/course-material/course-finder";
 
@@ -23,14 +21,13 @@ const dropdownSelector =
 const addButtonSelector =
 "body > div:nth-child(3) > div.main__inner-wrapper > div.yCmsContentSlot.course-finder-center-content-component > div > div > div > div.bned-cf-container > div.bned-course-finder-form-wrapper > form > div > div.bned-buttons-wrapper > div.bned-block-actions > a.js-bned-new-course.btn.btn-secondary.btn-block";
 
-var currentDepartmentIndex = 20;
+var currentDepartmentIndex = 20; // Select course (20th is ES Department)
 var departmentScope = 0;
 var courseScope = 0;
 var sectionScope = 0;
 var sectionList = [];
+var divNumberScope = 2; // The first div-child is 2
 
-// The first div-child is 2
-var divNumberScope = 2;
 // Test Fall and Winter terms
 async function scopeDropDown(page, term, divNumber) {
   await sleep(100);
@@ -158,6 +155,7 @@ async function addAnotherCourseButton(page, selector, times) {
   await page.waitForSelector(selector);
   await clickMultipleTimes(page, selector, times);
 }
+
 // Plus two only needed first time
 async function selectTerm(page, term) {
   let isFirstExecution = true;
@@ -319,6 +317,84 @@ async function selectionSection(page) {
   divNumberScope = activeDivNumberScope + 1;
 }
 
+async function textbookInfoCopier(page) {
+  await page.waitForSelector("div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > h2 > a > span:nth-child(1)", {timeout: 1000000}); // Term
+  let term = await page.$eval(
+    "div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > h2 > a > span:nth-child(1)",
+    (element) => element.textContent
+  );
+  
+  await page.waitForSelector("div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > h2 > a > span:nth-child(2)"); // Department
+  let department = await page.$eval(
+    "div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > h2 > a > span:nth-child(2)",
+    (element) => element.textContent
+  );
+
+  await page.waitForSelector("div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > h2 > a > span:nth-child(3)"); // Course
+  let course = await page.$eval(
+    "div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > h2 > a > span:nth-child(3)",
+    (element) => element.textContent
+  );
+
+  await page.waitForSelector("div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > h2 > a > span:nth-child(4)"); // Section
+  let section = await page.$eval(
+    "div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > h2 > a > span:nth-child(4)",
+    (element) => element.textContent
+  );
+  
+  // Remove the "Professor" from the string
+  await page.waitForSelector("div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > div > div > span"); // Professor
+  let professor = await page.$eval(
+    "div.js-bned-course-material-list-cached-content-container > div:nth-child(2) > div > div.bned-collapsible-head > div > div > span",
+    (element) => element.textContent
+  );
+
+  await page.waitForSelector("div.bned-item-details-container > div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > h3 > a > span"); // Textbook LOOK AT ME
+  let textbook = await page.$eval(
+    "div.bned-item-details-container > div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > h3 > a > span",
+    (element) => element.textContent
+  );
+  /*
+  FIXME: The authors section as the following format issue, fix in csv
+              By
+     hess, darrel / tasa, dennis g.
+
+    Remove the spaces and new line
+    Remove the By
+    Maybe remove the / and maybe replace with a comma?
+  */
+  await page.waitForSelector("div.bned-item-details-container > div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > div > span"); // Author()
+  let authors = await page.$eval(
+    "div.bned-item-details-container > div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > div > span",
+    (element) => element.textContent
+  );
+
+  await page.waitForSelector("div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > div > div:nth-child(2) > span.value"); // Edition
+  let edition = await page.$eval(
+    "div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > div > div:nth-child(2) > span.value",
+    (element) => element.textContent
+  );
+
+  await page.waitForSelector("div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > div > div:nth-child(3) > span.value"); // Publisher
+  let publisher = await page.$eval("div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > div > div:nth-child(3) > span.value", (element) => element.textContent);
+
+  await page.waitForSelector("div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > div > div:nth-child(4) > span.value"); // ISBN
+  let isbn = await page.$eval("div.bned-item-details-wp.js-item-details-wp > div.bned-item-attributes-notes-wp > div.bned-name-attributes-wp > div > div:nth-child(4) > span.value", (element) => element.textContent);
+
+  // Prices will go here, but that's complicated and my brain go BRR
+  
+  console.log("Term:", term);
+  console.log("Department:", department);
+  console.log("Course:", course);
+  console.log("Section:", section);
+  console.log("Professor:", professor);
+  console.log("Textbook:", textbook);
+  console.log("Authors:", authors);
+  console.log("Edition:", edition);
+  console.log("Publisher:", publisher);
+  console.log("ISBN:", isbn);
+}
+
 async function createPage() {
   const browser = await puppeteer.launch({
     headless: false,
@@ -345,6 +421,7 @@ async function selectionPage(page) {
   await selectionSection(page);
   console.log("Div Location: ", divNumberScope);
   console.log("Current Department Index: ", currentDepartmentIndex);
+  await textbookInfoCopier(page);
 }
 
 // fs.appendFile(filePath, `${Term},${Department},${Course},${Section},${Textbook},${Textbook2},${Textbook3}\n`);
