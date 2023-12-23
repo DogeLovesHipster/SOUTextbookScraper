b0cf1a07397645d05159360b85b46d16fca989a7
Sergio <shadow41402@gmail.com>
2023-12-22 21:49:29 -0800

    Added useful delay for function

--- utils/waitForSelectorAndPerformAction.js
+++ utils/waitForSelectorAndPerformAction.js
@@ -1,14 +1,15 @@
-async function waitForSelectorAndPerformAction(page, selector, action) {
-    await page.waitForSelector(selector);
-  
-    if (action === 'click') {
+async function waitForSelectorAndPerformAction(page, selector, action, delay = 0) {
+  await page.waitForSelector(selector);
+  await page.waitForTimeout(delay);
+
+  if (action === 'click') {
       await page.click(selector);
-    } else if (action === 'getText') {
+  } else if (action === 'getText') {
       const elementText = await page.$eval(selector, (element) => element.textContent);
       return elementText;
-    }
   }
-  
-  module.exports = { 
-    waitForSelectorAndPerformAction 
+}
+
+module.exports = { 
+  waitForSelectorAndPerformAction 
 };
